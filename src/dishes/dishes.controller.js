042e8de47da6709e098b7c0f7bf640e0962f8914
jclare93 <85895069+jclare93@users.noreply.github.com>
2021-10-11 12:23:54 -0700

    GrubDash Project
    creating of a project that manages the  ability to read, add, edit, and delete orders and dishes

---
+++ src/dishes/dishes.controller.js
@@ -0,0 +1,152 @@
+const path = require("path");
+const { runInNewContext } = require("vm");
+
+// Use the existing dishes data
+const dishes = require(path.resolve("src/data/dishes-data"));
+
+// Use this function to assign ID's when necessary
+const nextId = require("../utils/nextId");
+
+// TODO: Implement the /dishes handlers needed to make the tests pass
+function dishIdIsValid (req, res, next) {
+    const {dishId} = req.params
+    const foundDish = dishes.find((dish) => dish.id === dishId)
+    if (foundDish) {
+        res.locals.dish = foundDish
+        next()
+    } else {
+        next({
+            status: 404,
+            message: `Dish does not exist: ${dishId}`
+        })
+    }
+}
+
+//makes sure the ids are valid for the update
+function updateIsValid (req, res, next){
+    const {dishId} = req.params
+    const {data: {id} = {}} = req.body
+    if (id){
+        if(id === dishId){
+            return next()
+        }
+        else{
+           return next({
+                status: 400, 
+                message: `Dish id does not match route id. Dish: ${id}, Route: ${dishId}`
+            })
+        }
+    }
+    return next()
+}
+
+//checks dish name validity
+function nameIsValid (req, res, next) {
+    const { data: { name } = {} } = req.body;
+    if (name && name.length > 0) {
+    return next();
+    }
+    next({
+    status: 400,
+    message: "Dish must include a name",
+    });
+}
+
+//validates description 
+function descriptionIsValid (req, res, next) {
+    const { data: { description } = {} } = req.body;
+    if (description && description.length > 0) {
+    return next();
+    }
+    next({
+    status: 400,
+    message: "Dish must include a description",
+    });
+}
+
+//validates image url
+function imageUrlIsValid (req, res, next) {
+    const { data: { image_url } = {} } = req.body;
+    if (image_url && image_url.length > 0) {
+    return next();
+    }
+    next({
+    status: 400,
+    message: "Dish must include a image_url",
+    });
+}
+
+//checks to make sure a price for the dish is valid
+function priceIsValid (req, res, next){
+    const { data: { price } = {} } = req.body;
+    if (!Number.isInteger(price)){
+        next({
+            status: 400,
+            message: "Dish must have a price that is an integer greater than 0",
+        })
+    }
+    if (price && price > 0) {
+    return next();
+    }
+    if (price <= 0){
+        next({
+        status: 400,
+        message: "Dish must have a price that is an integer greater than 0",
+        })
+    }   
+    if (!price) {
+        next({
+            status: 400,
+            message: "Dish must include a price",
+        })
+    }
+}
+//create function for adding an new dish
+function create(req, res, next) {
+    const {data: {name, description, price, image_url} = {}} = req.body;
+    const newId = nextId()
+    const newDish = {
+        id: newId,
+        name, 
+        description, 
+        price,
+        image_url 
+    }
+    dishes.push(newDish)
+    res.status(201).json({ data: newDish });
+}
+
+//reads the dish data given a dishId
+function read(req, res, next){
+    res.json({ data: res.locals.dish })
+}
+
+function update(req, res, next) {
+    const dish = res.locals.dish
+    const {data: {name, description, price, image_url} = {}} = req.body;
+    dish.name = name;
+    dish.description = description;
+    dish.price = price;
+    dish.image_url = image_url;
+    res.json({data: dish})
+}
+
+//don't need to destroy
+/*function destroy (req, res, next) {
+    const {dishId} = req.params
+    const index = dishes.findIndex((dish) => dish.id = dishId)
+    dishes.splice(index, 1)
+    res.sendStatus(204);
+}*/
+
+//lists all dishes
+function list(req, res){
+    res.json({data: dishes})
+}
+
+module.exports = {
+create: [nameIsValid, descriptionIsValid, imageUrlIsValid, priceIsValid, create], 
+read: [dishIdIsValid, read], 
+update: [dishIdIsValid, nameIsValid, descriptionIsValid, imageUrlIsValid, priceIsValid, updateIsValid, update], 
+list,
+}
---
+++ src/orders/orders.controller.js
@@ -0,0 +1,184 @@
+const path = require("path");
+const { resourceLimits } = require("worker_threads");
+
+// Use the existing order data
+const orders = require(path.resolve("src/data/orders-data"));
+
+// Use this function to assigh ID's when necessary
+const nextId = require("../utils/nextId");
+
+// TODO: Implement the /orders handlers needed to make the tests pass
+
+//makes sure we are given a vlid :orderId
+function orderIdIsValid(req, res, next){
+    const { orderId } = req.params
+    let {data: {id} = {}} = req.body
+    const foundOrder = orders.find((order) => order.id === orderId)
+    if (foundOrder)  {
+        if (id){
+        res.locals.order = foundOrder
+        return next()} 
+        else {
+            id = orderId
+            res.locals.order = foundOrder
+            return next()
+        }
+        } else {
+        return next({
+            status: 404,
+            message: `Order does not exist: ${orderId}`
+        })
+    }
+}
+
+//validates dish quantity
+function dishQuantityIsValid (req, res, next) {
+    const {data: {dishes} = {}} = req.body
+    for (let i = 0; i < dishes.length; i++){
+        if (!Number.isInteger(dishes[i].quantity) || !dishes[i].quantity >0){
+            return next({
+                status: 400,
+                message: `Dish ${i} must have a quantity that is an integer greater than 0`
+            })
+        }
+    }
+    return next()
+}
+
+//validates the address
+function deliverToIsValid (req, res, next) {
+    const { data: { deliverTo } = {} } = req.body;
+    if (deliverTo && deliverTo.length > 0) {
+    return next();
+    }
+    next({
+    status: 400,
+    message: "Order must include a deliverTo",
+    });
+}
+
+//checks to make sure mobile number is valid
+function mobileNumberIsValid (req, res, next) {
+    const { data: { mobileNumber } = {} } = req.body;
+    if (mobileNumber && mobileNumber.length > 0) {
+    return next();
+    }
+    next({
+    status: 400,
+    message: "Order must include a mobileNumber",
+    });
+}
+
+//checks to make sure the dishes data piece is valid and is an array
+function dishesIsValid(req, res, next){
+    const {data: {dishes} = {}} = req.body
+    if (dishes) {
+        if (Array.isArray(dishes) && dishes.length > 0) {
+            return next()
+        } else {
+            return next({
+                status: 400, 
+                message: 'Order must include at least one dish'
+            })
+        }
+    } else{
+        return next({
+            status: 400, 
+            message: 'Order must include a dish'
+        })
+    }
+}
+
+//makes sure we are given a valid status
+function statusIsValid(req, res, next) {
+    const {data: {status} = {}} = req.body
+    if (status === "delivered"){
+        return next({
+            status: 404, 
+            message: 'A delivered order cannot be changed'
+        })
+    }
+   if (status === ('pending' || 'preparing' || 'out-for-delivery')){
+       return next()
+   } else {
+       return next({
+           status: 400, 
+           message: 'Order must have a status of pending, preparing, out-for-delivery, delivered'
+       })
+   }
+}
+
+//checks if id is given. if id is given then it make sure it matches
+function routeOrderValidation(req, res, next) {
+    const {orderId} = req.params
+    const {data: {id} = {}} = req.body
+    if (orderId === id || !id) {
+        return next()
+    } else {
+        return next({
+            status: 400,
+            message: `Order id does not match route id. Order: ${id}, Route: ${orderId}.`
+        })
+    }
+}
+
+//creates order given order details + assigns new order id
+function create(req, res, next) {
+    const {data: {deliverTo, mobileNumber, status, dishes} = {}} = req.body
+    const newId = nextId()
+    const newOrder = {
+        id: newId,
+        deliverTo,
+        mobileNumber,
+        status,
+        dishes
+    }
+    orders.push(newOrder)
+    res.status(201).json({data: newOrder})
+}
+
+//response with the data from the order given order id
+function read(req, res, next) {
+    res.json({data: res.locals.order})
+}
+
+
+//updates the old order with the new order details
+function update (req, res, next) {
+    const order = res.locals.order
+    const {data: {deliverTo, mobileNumber, status, dishes} = {}} = req.body
+    order.deliverTo = deliverTo
+    order.mobileNumber = mobileNumber
+    order.status = status
+    order.dishes = dishes
+    res.json({data: order})
+}
+
+//checks if status is pending for order then deletes it from orders array
+function destroy(req, res, next) {
+    const {orderId} = req.params
+    const foundOrder = res.locals.order
+    if (foundOrder.status == 'pending') {
+    const index = orders.findIndex((order) => order.id === orderId)
+    orders.splice(index, 1)
+    res.sendStatus(204);}
+    else {
+        return next({
+            status: 400,
+            message: "An order cannot be deleted unless it is pending.",
+          });
+        }
+    }
+
+function list(req, res, next) {
+    res.json({data: orders})
+}
+
+
+module.exports = {
+    create: [deliverToIsValid, mobileNumberIsValid, dishesIsValid, dishQuantityIsValid, create], 
+    read: [orderIdIsValid, read], 
+    update: [deliverToIsValid, mobileNumberIsValid, dishesIsValid, dishQuantityIsValid, orderIdIsValid, routeOrderValidation, statusIsValid, update], 
+    delete: [orderIdIsValid, destroy],
+    list,
+    }
